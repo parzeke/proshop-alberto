9a0cc24f53dd3a9f51af29f5b9ab7d46bb0ae302
Parzeke <bertoncp@gmail.com>
2025-11-30 23:25:41 +0100

    navbar actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.jsx";
 function App() {
   return (
     <div>
-      {/* Header con Navbar */}
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Header/>
+      <main>
         <Container>
-          {/* Logo o nombre de la tienda */}
-          <Navbar.Brand href="/">Mi Tienda</Navbar.Brand>
-
-          {/* Links de navegación a la derecha */}
-          <Nav className="ms-auto">
-            <Nav.Link href="/cart">
-              <FaShoppingCart style={{ marginRight: '5px' }} />
-              Carrito
-            </Nav.Link>
-            <Nav.Link href="/login">
-              <FaUser style={{ marginRight: '5px' }} />
-              Usuario
-            </Nav.Link>
-          </Nav>
+          <h1>Welcome</h1>
         </Container>
-      </Navbar>
-
-      {/* Contenido principal */}
-      <main style={{ padding: '2rem', textAlign: 'center' }}>
-        <h1>Hola mundo</h1>
-        <p>¡Tu Navbar y los iconos funcionan correctamente!</p>
       </main>
     </div>
   );
